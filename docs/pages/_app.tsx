16aad24e4decee49a876ec57f0fcd8b7a7b9fd08
lcx12901 <wktl1991504424@gmail.com>
2023-02-19 16:34:02 +0800

    init

---
+++ docs/pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { ReactElement } from 'react'
+
+import type { AppProps } from 'next/app'
+
+import '../style.css'
+
+export default function Nextra({
+  Component,
+  pageProps
+}: AppProps): ReactElement {
+  return <Component {...pageProps} />
+}
